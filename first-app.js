3d9589bbca5298edf49503a482448d842f2a1c5d
Vaibhav Arora <varora@catalystone.com>
2019-11-12 20:12:17 +0530

    Section 4: combined result of all lessons

--- first-app.js
+++ first-app.js
@@ -3,6 +3,7 @@ const http = require("http");
 const routes = require("./routes");
 
 console.log(routes.someText);
+console.log('Hey');
 const server = http.createServer(routes.handler);
 
 server.listen(3000);
