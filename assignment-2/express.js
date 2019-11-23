e77c6899ab657b474ee7984f4f780756aec8c06b
Vaibhav Arora <varora@catalystone.com>
2019-11-23 19:47:44 +0530

    Lesson 68: submitting admin path prefix for it's routes

--- assignment-2/express.js
+++ assignment-2/express.js
@@ -7,7 +7,8 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes, shopRoutes);
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
   res.status(404).send('<h1>Page not found</h1>');
--- assignment-2/routes/admin.js
+++ assignment-2/routes/admin.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-  res.send(`<form action="/product" method="POST">
+  res.send(`<form action="/admin/product" method="POST">
     <input type="text" name="title" />
     <button type="submit">Add product</button>
   </form>`);
