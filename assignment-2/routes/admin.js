fa0c42f1d97899171bad78f7288fcada0a9613d9
Vaibhav Arora <varora@catalystone.com>
2019-11-25 20:02:50 +0530

    Lesson 69, 70, 71: Serving html from files instead of inline code

--- assignment-2/routes/admin.js
+++ assignment-2/routes/admin.js
@@ -1,11 +1,14 @@
+const path = require('path');
 const express = require('express');
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-  res.send(`<form action="/admin/product" method="POST">
-    <input type="text" name="title" />
-    <button type="submit">Add product</button>
-  </form>`);
+  // res.send(`<form action="/admin/product" method="POST">
+  //   <input type="text" name="title" />
+  //   <button type="submit">Add product</button>
+  // </form>`);
+
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 router.post('/product', (req, res, next) => {
--- assignment-2/routes/shop.js
+++ assignment-2/routes/shop.js
@@ -1,9 +1,11 @@
+const path = require('path');
+
 const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
   console.log('2nd use function');
-  res.send(`<h1>Default Page</h1>`);
+  res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router;
