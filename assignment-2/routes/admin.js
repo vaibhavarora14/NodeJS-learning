ede6110752b95916ee4b5b509bddd62e187ffd1a
Vaibhav Arora <varora@catalystone.com>
2020-01-19 17:04:11 +0530

    Lesson 72: committing changes taught

--- assignment-2/routes/admin.js
+++ assignment-2/routes/admin.js
@@ -2,13 +2,15 @@ const path = require('path');
 const express = require('express');
 const router = express.Router();
 
+const rootDir = require('../util/path');
+
 router.get('/add-product', (req, res, next) => {
   // res.send(`<form action="/admin/product" method="POST">
   //   <input type="text" name="title" />
   //   <button type="submit">Add product</button>
   // </form>`);
 
-  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post('/product', (req, res, next) => {
