a6650f3e3f94702770f92c8b685a72374a0538a5
Vaibhav Arora <varora@catalystone.com>
2020-02-09 15:58:30 +0530

    Section-6: main-layout pub file to share code which can be re-used among other pub files

--- section-6/routes/shop.js
+++ section-6/routes/shop.js
@@ -11,7 +11,8 @@ router.get('/', (req, res, next) => {
   const products = adminData.products;
   res.render('shop', {
     products,
-    docTitle: 'Shop'
+    pageTitle: 'Shop',
+    path: '/'
   });
 });
 
