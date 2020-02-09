49c89da998b6e809302048987cbf0fd566bab33b
Vaibhav Arora <varora@catalystone.com>
2020-02-09 22:19:37 +0530

    Section-7: finishing controllers startup work

--- section-7/routes/admin.js
+++ section-7/routes/admin.js
@@ -1,29 +1,12 @@
-const path = require('path');
-
 const express = require('express');
 
-const rootDir = require('../util/path');
-
 const router = express.Router();
-
-const products = [];
+const productsController = require('../controllers/products');
 
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  res.render('add-product', {
-    pageTitle: 'Add Product',
-    path: 'admin/add-product',
-    formCSS: true,
-    productCSS: true,
-    addProductActive: true
-  });
-});
+router.get('/add-product', productsController.getAddProduct);
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect('/');
-});
+router.post('/add-product', productsController.postAddProduct);
 
 exports.routes = router;
-exports.products = products;
