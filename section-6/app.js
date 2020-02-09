245552392154fff4106c220c1b3cbcebb3854143
Vaibhav Arora <varora@catalystone.com>
2020-02-09 19:31:26 +0530

    Section-6: Fixing issue of path in 404 page

--- section-6/app.js
+++ section-6/app.js
@@ -19,7 +19,8 @@ app.use(shopRoutes);
 
 app.use((req, res, next) => {
   res.status(404).render('404', {
-    pageTitle: 'Page Not Found'
+    pageTitle: 'Page Not Found',
+    path: '404'
   });
 });
 
